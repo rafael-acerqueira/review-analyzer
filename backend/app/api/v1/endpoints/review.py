8c4a35cfed35672a6d209fe4c64d21f03498ea66
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-05-15 07:57:48 -0300

    (backend): Sending review to Hugging Faces analyze

--- backend/app/api/v1/endpoints/review.py
+++ backend/app/api/v1/endpoints/review.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
-from schemas import ReviewRequest, ReviewResponse
+from app.schemas import ReviewRequest, ReviewResponse
 
+from app.services.sentiment_analysis_service import SentimentAnalysisService
 
 router = APIRouter()
 
 @router.post("/reviews")
 def analyze_review(request: ReviewRequest) -> ReviewResponse:
+    sentiment, polarity  = SentimentAnalysisService.analyze(request.text)
     return ReviewResponse(
         text='Test',
-        sentiment='Positive',
-        polarity=0.5,
+        sentiment=sentiment,
+        polarity=polarity,
         suggestion="Should improve this feedback for X reasons",
         status="Accept",
         feedback="It's a good pillow"
