370e75f6c3e59382d2216de3efc26ff79b5cf9f2
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-05-16 17:32:42 -0300

    (backend): Pick another model to text generation

--- backend/app/core/clients.py
+++ backend/app/core/clients.py
@@ -1,21 +1,33 @@
-from pyexpat.errors import messages
-
+from dotenv import load_dotenv
 from transformers import pipeline
-import openai
+from huggingface_hub import InferenceClient
+from functools import lru_cache
 import os
+
+load_dotenv()
 
 sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
+HF_MODEL = "microsoft/phi-4"
 
-def call_llm(prompt: str) -> str:
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are an expert in evaluating reviews."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=150,
-        temperature=0.3
-    )
-    return response['choices'][0]['message']['content']
+
+
+client = InferenceClient(
+    provider="hf-inference",
+    api_key=HF_TOKEN,
+)
+
+@lru_cache(maxsize=128)
+def call_llm(user_prompt: str) -> str:
+    try:
+        completion = client.chat.completions.create(
+            model=HF_MODEL,
+            messages=[{"role": "user", "content": user_prompt}],
+            temperature=0.3,
+            max_tokens=150,
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        print(f"[LLM ERROR] {e}")
+        return '{"status": "Rejected", "feedback": "AI error", "suggestion": ""}'
