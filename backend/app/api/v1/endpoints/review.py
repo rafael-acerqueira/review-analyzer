af3f2155975bb1bf20a531e9a97d4f8572ff5c09
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-05-16 17:39:23 -0300

    (backend): Add /reviews endpoint and its tests

--- backend/app/api/v1/endpoints/review.py
+++ backend/app/api/v1/endpoints/review.py
@@ -2,17 +2,20 @@ from fastapi import APIRouter
 from app.schemas import ReviewRequest, ReviewResponse
 
 from app.services.sentiment_analysis_service import SentimentAnalysisService
+from app.services.suggestion_service import SuggestionService
 
 router = APIRouter()
 
 @router.post("/reviews")
 def analyze_review(request: ReviewRequest) -> ReviewResponse:
     sentiment, polarity  = SentimentAnalysisService.analyze(request.text)
+    quality = SuggestionService.evaluate_review(request.text)
+
     return ReviewResponse(
-        text='Test',
+        text=request.text,
         sentiment=sentiment,
         polarity=polarity,
-        suggestion="Should improve this feedback for X reasons",
-        status="Accept",
-        feedback="It's a good pillow"
+        suggestion=quality['suggestion'],
+        status=quality['status'],
+        feedback=quality['feedback']
     )
