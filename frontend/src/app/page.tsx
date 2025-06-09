85801c7231a0f9f407fee51204da37caef5c3e50
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-06-08 21:09:20 -0300

    (frontend): Redirect to login page if user access homepage and is not authorized

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,23 @@
+'use client'
+
+import { useSession } from "next-auth/react";
 import ReviewForm from "./review/components/ReviewForm";
+import { useRouter } from 'next/navigation'
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.push('/login')
+    }
+  }, [status, router])
+
+  if (status === 'loading') return <p>Loading...</p>
+
   return (
     <ReviewForm />
   );
