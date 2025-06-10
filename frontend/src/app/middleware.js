9670e329127b5e672a2534c24a4465d53410b1e2
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-06-10 10:22:05 -0300

    (frontend): Create a middleware to manage redirects based on authentication

---
+++ frontend/src/app/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../lib/auth/authOptions";
+
+export async function middleware(req) {
+  const session = await getServerSession(authOptions);
+
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (session.user?.role !== "admin") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/*"],
+};
