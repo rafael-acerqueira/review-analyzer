93f8c0c60e99cd99dfa8cf4a5956dc35a52549e2
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-06-15 18:15:35 -0300

    (backend): Fix Session in the create_user

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,8 +8,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
-SessionLocal = Session
-
 def get_session():
     with Session(engine) as session:
         yield session
--- backend/create_test_user.py
+++ backend/create_test_user.py
@@ -1,18 +1,17 @@
-from app.database import engine, SessionLocal
+from app.database import engine
 from app.models.user import User
 from app.security import hash_password
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Session
 
 SQLModel.metadata.create_all(engine)
 
-db = SessionLocal()
-user = User(
-    email="user@example.com",
-    hashed_password=hash_password("password123"),
-    provider="credentials",
-    role="user"
-)
-db.add(user)
-db.commit()
-db.close()
-print("Test user created.")
+with Session(engine) as session:
+    user = User(
+        email="user@example.com",
+        hashed_password=hash_password("password123"),
+        provider="credentials",
+        role="user"
+    )
+    session.add(user)
+    session.commit()
+    print("Test user created.")
